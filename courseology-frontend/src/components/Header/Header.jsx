7c15f0b6d8098d00d1ae76067369cf2e85a09306
aiman.wahizam <aiman.wahizam@nology.io>
2022-07-28 10:16:59 +0100

    added link to homepage in header

--- courseology-frontend/src/components/Header/Header.jsx
+++ courseology-frontend/src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header.scss";
+import { Link } from "react-router-dom";
+
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__name">_courseology</div>
+      <Link to="/">
+        <div className="header__name">_courseology</div>
+      </Link>
     </header>
   );
 };
